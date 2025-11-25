e3d5f9f7ab2793fdeef73f4b251c41627cf6fa70
Bryson Villanueva <villanueva.375803@legazpi.sti.edu.ph>
2025-11-25 22:21:15 +0800

    Test test

---
+++ Test3/Data/Services/TransactionService.cs
@@ -0,0 +1,41 @@
+﻿using MongoDB.Driver;
+using Test3.Data.Models;
+
+namespace Test3.Data.Services
+{
+    public class TransactionService
+    {
+        private readonly IMongoCollection<TransactionHistory> _transactions;
+
+        public TransactionService(IMongoDatabase database)
+        {
+            _transactions = database.GetCollection<TransactionHistory>("TransactionHistory");
+        }
+
+        public async Task CreateTransactionAsync(TransactionHistory transaction)
+        {
+            await _transactions.InsertOneAsync(transaction);
+        }
+
+        public async Task<List<TransactionHistory>> GetTransactionsByLandlordAsync(string landlordId)
+        {
+            return await _transactions.Find(x => x.LandlordId == landlordId)
+                                    .SortByDescending(x => x.PaymentDate)
+                                    .ToListAsync();
+        }
+
+        public async Task<List<TransactionHistory>> GetTransactionsByUserAsync(string userId)
+        {
+            return await _transactions.Find(x => x.UserId == userId)
+                                    .SortByDescending(x => x.PaymentDate)
+                                    .ToListAsync();
+        }
+
+        public async Task<TransactionHistory?> GetLatestTransactionAsync(string userId)
+        {
+            return await _transactions.Find(x => x.UserId == userId)
+                                    .SortByDescending(x => x.PaymentDate)
+                                    .FirstOrDefaultAsync();
+        }
+    }
+}
